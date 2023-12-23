17c76b5b15a85dfd86204538754e8c05a00c39ef
Arnaud Avocat Gros <arnaud.avocatgros18@gmail.com>
2023-12-23 14:25:17 +0100

    supprimer le travail de tom qui est pas le notre

--- include/server.h
+++ include/server.h
@@ -1,20 +1,5 @@
-#ifndef SERVER_H
-#define SERVER_H
+int startserver(int port);
+int stopserver();
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-
-extern unsigned short address;
-extern unsigned short g_4040b2;
-extern unsigned int g_4040b4;
-extern unsigned int server_fd;
-extern unsigned int addrlen;
-extern unsigned int new_socket;
-extern unsigned long long valread;
-
-long long startserver(unsigned long a0);
-long long getmsg(char msg_read[1024]);
-long long stopserver();
-
-#endif  // SERVER_H
+/* read message sent by client */
+int getmsg(char msg_read[1024]);
